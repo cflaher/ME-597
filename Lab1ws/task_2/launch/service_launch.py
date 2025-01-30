2a9943fee1b17ef35b4ce92f9064f965c5376955
Cian <cflaherty310@gmail.com>
2025-01-30 03:01:53 -0500

    reverted launch, updated setup.py

--- Lab1ws/task_2/launch/service_launch.py
+++ Lab1ws/task_2/launch/service_launch.py
@@ -1,32 +1,18 @@
-from setuptools import find_packages, setup
-import os
-from glob import glob
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-package_name = 'task_2'
-
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # This line ensures launch files are properly installed
-        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*'))
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='Task 2 package',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'talker = task_2.publisher_node:main',
-            'service = task_2.service_node:main',
-            'client = task_2.client_node:main',
-        ],
-    },
-)
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='task_2',
+            executable='talker',
+            name='publisher_node',
+            output='screen'
+        ),
+        Node(
+            package='task_2',
+            executable='service',
+            name='service_node',
+            output='screen'
+        )
+    ])
--- Lab1ws/task_2/setup.py
+++ Lab1ws/task_2/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'task_2'
 
@@ -10,20 +12,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # This line ensures launch files are properly installed
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='me597',
-    maintainer_email='me597@todo.todo',
-    description='TODO: Package description',
-    license='Apache-2.0',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Task 2 package',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = task_2.publisher_member_function:main',
-            'listener = task_2.subscriber_member_function:main',
-            'service = task_2.service:main',
-            'client = task_2.client:main',
-        ]
+            'talker = task_2.publisher_node:main',
+            'service = task_2.service_node:main',
+            'client = task_2.client_node:main',
+        ],
     },
 )
