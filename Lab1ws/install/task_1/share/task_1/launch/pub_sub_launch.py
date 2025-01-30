edca15b13b672118a0b4bd4c5158df0e0265f224
Cian <cflaherty310@gmail.com>
2025-01-30 00:29:54 -0500

    updated Cmakelists.txt

--- Lab1ws/install/task_1/share/task_1/launch/pub_sub_launch.py
+++ Lab1ws/install/task_1/share/task_1/launch/pub_sub_launch.py
@@ -3,6 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='task_1', executable='minimal_publisher', output='screen'),
-        Node(package='task_1', executable='minimal_subscriber', output='screen'),
+        Node(package='task_1', executable='talker', output='screen'),
+        Node(package='task_1', executable='listener', output='screen'),
     ])
