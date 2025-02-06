547ff8580c8b1f1e81804ab86d4e74459aee5e86
Cian <cflaherty310@gmail.com>
2025-02-06 12:32:43 -0500

    update pid_controller

--- lab2ws/install/task_3/lib/python3.10/site-packages/task_3/pid_controller.py
+++ lab2ws/install/task_3/lib/python3.10/site-packages/task_3/pid_controller.py
@@ -3,21 +3,20 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 from std_msgs.msg import Float32
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+from task_3_interfaces.sensor_msgs import LaserScan
+from geometry_msgs import Twist
 
-class PidController(Node):
+class pid_controller(Node):
 
     def __init__(self):
         super().__init__('pid_controller')
 
-        # pid params
+        # global params
         self.target_dist = 0.35
         self.kp = 0.9
         self.ki = 0.1
         self.kd = 0.1
 
-        # subscriber
         self.subscription = self.create_subscription(
             LaserScan,
             'scan',
@@ -25,35 +24,34 @@ class PidController(Node):
             10)
         self.subscription  # prevent unused variable warning
 
-        #publisher
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.timer_period = 0.1  # seconds
-        self.timer = self.create_timer(self.timer_period, self.control)
+        self.timer = self.create_timer(self.timer_period, self.control_loop)
 
-        # pid variables
         self.current_distance = 0
-        self.error = 0
-        self.integral_error = 0
-        self.prev_error = 0
-        self.derivative_error = 0
-        self.output = 0
 
     def listener_callback(self, msg):
         self.current_distance = msg.ranges[0]
 
-    def control(self):
-        # calculate errors
+    def control(self, cmd):
+    
+        self.error = 0
+        self.integral_error = 0
+        self.error_last = 0
+        self.derivative_error = 0
+        self.output = 0
+
         self.error = self.target_dist - self.current_distance
         self.integral_error += self.error * self.timer_period
-        self.derivative_error = (self.error - self.prev_error) / self.timer_period
+        self.derivative_error = (self.error - self.error_last) / self.timer_period
         self.prev_error = self.error
         self.output = self.kp * self.error + self.ki * self.integral_error + self.kd * self.derivative_error
         
         cmd = Twist()
         if self.output > 0.15:
-            self.output = 0.15
+            self.output -= .1
         elif self.output < -0.15:
-            self.output = -0.15
+            self.output += .01
         
         cmd.linear.x = self.output
         cmd.angular.z = 0.0
@@ -61,13 +59,10 @@ class PidController(Node):
 
         self.prev_error = self.error
     
-def control_loop(self):
-        self.control()
-
 def main(args=None):
     rclpy.init(args=args)
 
-    pid_controller = PidController()
+    pid_controller = pid_controller()
 
     rclpy.spin(pid_controller)
 
@@ -80,3 +75,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+    
