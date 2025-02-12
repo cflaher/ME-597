4d5090435aa811c59554f4c364bd37fb32a7d2a9
Cian <cflaherty310@gmail.com>
2025-02-12 17:40:04 -0500

    removed /robot

--- lab2ws/src/task_3/task_3/pid_controller.py
+++ lab2ws/src/task_3/task_3/pid_controller.py
@@ -20,13 +20,13 @@ class PidController(Node):
         # subscriber
         self.subscription = self.create_subscription(
             LaserScan,
-            '/robot/scan',
+            '/scan',
             self.lidar_callback,
             10)
         self.subscription  # prevent unused variable warning
 
         #publisher
-        self.publisher = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.timer_period = 0.1  # seconds
         self.timer = self.create_timer(self.timer_period, self.control)
 
